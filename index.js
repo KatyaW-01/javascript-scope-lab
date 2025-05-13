3d160935a6b9c14257bb78e47a1e1a83ab33130d
Katya Weicht <katya.weicht@gmail.com>
2025-05-12 22:17:50 -0600

    Write addBurger function

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Write your solution in this file!\n
 const burgers = ['Hamburger', 'Cheeseburger']
 let featuredDrink = 'Strawberry Milkshake'
+
+function addBurger() {
+  const newBurger = 'Flatburger'
+  burgers.push(newBurger)
+}
